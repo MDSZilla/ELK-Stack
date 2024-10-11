bb3e741aff79baebc77857d07c04e91f22f062a0
MDSZilla <mdszilla@gmail.com>
2024-10-11 17:42:34 +0500

    Added Comments for Better Understanding

--- node/index.ts
+++ node/index.ts
@@ -5,10 +5,13 @@ import * as http from "http";
 import * as winston from "winston";
 
 const app = express();
+
+//Winston is used to create log files from a Node Server
 const logger = winston.createLogger({
-    transports: [
+    transports: [ //Transports are used to define what kind of data to store; file, stream etc.
         new winston.transports.File({filename: "../docker/logstash/logs/testlog.log"})
     ],
+    //Format is used to define what formatting the log file will follow
     format: winston.format.combine(
         winston.format.timestamp(), winston.format.json()
     ),
@@ -26,7 +29,7 @@ const server = http.createServer(app);
 const PORT = 3001;
 
 app.get('/', (req, res) => {
-    logger.info("Server Running")
+    logger.info("Server Running") //Logger.info writes to the log file
     res.write(`<h1>Server Running on Port : ${PORT}</h1>`);
     res.end();
 });
